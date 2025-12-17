5a049191cf01adb7a1bfe4cfca016a53cd020aa3
HARISH KANNAN N <harishkannann05@gmail.com>
2025-12-17 19:42:19 +0530

    Time: 0 ms (100%), Space: 44 MB (77.13%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -1,15 +1,15 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
-        int sum = 0, ans = 0;
+        int sum = 0, max = 0;
         for (int i = 0; i <accounts.length; i++) {
             sum=0;
             for (int j = 0; j < accounts[i].length; j++) {
                 sum = sum + accounts[i][j];
             }
-            if (ans < sum) {
-                ans = sum;
+            if (max < sum) {
+                max = sum;
             }
         }
-        return ans;
+        return max;
     }
 }

b7073b2bb558c2a63f90dd8198de907d8d6d2b0a
HARISH KANNAN N <harishkannann05@gmail.com>
2025-12-30 22:44:07 +0530

    Time: 2 ms (67.77%), Space: 85 MB (74.81%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        boolean inc = false;
+        boolean dec = false;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] < nums[i + 1]) {
+                inc = true;
+            }
+            if (nums[i] > nums[i + 1]) {
+                dec = true;
+            }
+        }
+
+        return !(inc && dec);
+    }
+}

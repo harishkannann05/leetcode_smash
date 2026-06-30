64cf737844e55ba908728485e4b8ad8bb8916d8c
HARISH KANNAN N <harishkannann05@gmail.com>
2026-06-30 14:13:31 +0530

    Time: 13 ms (5.6%), Space: 49.1 MB (15.42%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -3,21 +3,9 @@ class Solution {
         int index=nums1.length+nums2.length;
         int pos=0;
         int[] array=new int[index];
-        System.out.println(index);
-        // for(int i=0;i<index;i++){
-        //     if(i==nums1.length){
-        //         pos=0;
-        //     }
-        //     if(i<nums1.length){
-        //         array[i]=nums1[pos];
-        //         pos++;
-        //     }
-        // }
-
         System.arraycopy(nums1,0,array,0,nums1.length);
         System.arraycopy(nums2,0,array,nums1.length,nums2.length);
         Arrays.sort(array);
-        System.out.println(Arrays.toString(array)+index/2);
         if(index%2==0){
             int sum=(array[index/2]+array[(index/2)-1]);
                     System.out.println(sum);
